04f3f05a80d0a0a4b0435f32fe99b6e3fa327fb7
Zaden Connell <zaden.connell@gmail.com>
2022-07-25 09:30:48 -0400

    Functions practice program

---
+++ Section 2/Functions/Practice_Functions/Practice_Functions/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  Practice_Functions
+//
+//  Created by Zaden Connell on 7/25/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b){
+    int c;
+    c = a + b;
+    
+    return c;
+}
+
+int main() {
+    int num1 = 10, num2 = 15, sum;
+    
+    sum = add(num1, num2);
+    
+    cout << "Sum is " << sum << endl;
+    
+    return 0;
+}
