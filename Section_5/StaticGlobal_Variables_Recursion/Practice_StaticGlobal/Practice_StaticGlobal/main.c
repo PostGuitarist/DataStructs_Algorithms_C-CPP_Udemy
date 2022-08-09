75c9b8aeca06aa93917ec399133a3db97807acfd
Zaden Connell <zaden.connell@gmail.com>
2022-08-09 09:50:04 -0400

    Static and global variables in recursion practice C program

---
+++ Section_5/StaticGlobal_Variables_Recursion/Practice_StaticGlobal/Practice_StaticGlobal/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  Practice_StaticGlobal
+//
+//  Created by Zaden Connell on 8/9/22.
+//
+
+#include <stdio.h>
+
+int fun(int n) {
+    if(n > 0) {
+        static int x = 0;
+        x++;
+        return fun(n-1) + x;
+    }
+    return 0;
+}
+
+int main() {
+    int r;
+    r = fun(5);
+    printf("%d\n", r);
+    
+    r = fun(5);
+    printf("%d\n", r);
+    
+    return 0;
+}
